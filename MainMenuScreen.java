e8d8ab9978e345e9238a1601238e5d2c495ee475
razimograbi <razi@mograbi.net>
2017-06-14 19:38:22 +0300

    Create MainMenuScreen.java

---
+++ MainMenuScreen.java
@@ -0,0 +1,90 @@
+ public class MainMenuScreen implements Screen {
+    private static final int EXIT_BUTTON_WIDTH = 250;
+    private static final int EXIT_BUTTON_HEIGHT = 120;
+    private static final int PLAY_BUTTON_wIDTH = 300;
+    private static final int PLAY_BUTTON_HEIGHT = 120;
+    private static final int EXIT_BUTTON_Y = 100;
+    private static final int PLAY_BUTTON_Y = 230;
+    private BitmapFont made;
+    MyGame2 game;
+
+
+    Texture PlayButtonActive;
+    Texture img;
+    Texture PlayButtonInactive;
+    Texture ExitButtonActive;
+    Texture ExitButtonInactive;
+
+    public MainMenuScreen(MyGame2 game){
+        this.game = game;
+        made = new BitmapFont();
+        made.getData().setScale(3,3);
+        img = new Texture("video-game-background-1405076_960_720.png");
+        PlayButtonActive = new Texture("play_button_active121.png");
+        PlayButtonInactive = new Texture("play_button_inactive.png");
+        ExitButtonActive = new Texture("exit_button_active.png");
+        ExitButtonInactive = new Texture("exit_button_inactive.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(img , 0 , 0);
+        GlyphLayout madelayout = new GlyphLayout(made, "Made by Razi");
+        made.draw(game.batch , madelayout ,MyGame2.WIDTH / 2 -madelayout.width / 2 , MyGame2.HEIGHT - madelayout.height - 200 );
+        int middle = MyGame2.WIDTH / 2 - EXIT_BUTTON_WIDTH / 2;
+        if (game.cam.getInputInGameWorld().x < middle + EXIT_BUTTON_WIDTH && game.cam.getInputInGameWorld().x > middle && MyGame2.HEIGHT - game.cam.getInputInGameWorld().y < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && MyGame2.HEIGHT - game.cam.getInputInGameWorld().y > EXIT_BUTTON_Y) {
+            game.batch.draw(ExitButtonActive, middle, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                Gdx.app.exit();
+            }
+
+        }else {
+            game.batch.draw(ExitButtonInactive , middle , EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH , EXIT_BUTTON_HEIGHT);
+        }
+        middle = MyGame2.WIDTH / 2 - PLAY_BUTTON_wIDTH / 2;
+        if (game.cam.getInputInGameWorld().x < middle + PLAY_BUTTON_wIDTH && game.cam.getInputInGameWorld().x > middle && MyGame2.HEIGHT - game.cam.getInputInGameWorld().y < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && MyGame2.HEIGHT - game.cam.getInputInGameWorld().y > PLAY_BUTTON_Y) {
+            game.batch.draw(PlayButtonActive, middle, PLAY_BUTTON_Y, PLAY_BUTTON_wIDTH, PLAY_BUTTON_HEIGHT);
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new MainGameScreen(game));
+            }
+
+        }else {
+            game.batch.draw(PlayButtonInactive, middle, PLAY_BUTTON_Y , PLAY_BUTTON_wIDTH , PLAY_BUTTON_HEIGHT);
+        }
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
